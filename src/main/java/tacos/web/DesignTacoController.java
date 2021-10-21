aa981b2ebc1cb740c91e98b8b106ad7cf75410a6
Christian Scholten <Christian.Scholten@hksinformatik.de>
2021-10-21 16:59:59 +0200

    'POST' für 'TacoDesignController', dazugehöriger 'IngredientbyIdConverter', 'OrderController', 'WebConfig' für 'home.html' sowie dazugehörige Templates hinzugefügt

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -1,20 +1,16 @@
 package tacos.web;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
-
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 import tacos.Ingredient;
 import tacos.Ingredient.Type;
 import tacos.Taco;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Controller
@@ -39,18 +35,27 @@ public class DesignTacoController {
 
         Type[] types = Ingredient.Type.values();
         for (Type type : types) {
-            model.addAttribute(type.toString().toLowerCase(),
-                    filterByType(ingredients, type));
+            // Bsp.: Ingredient.sauce: ["Salsa", "Sour Cream"]
+            model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
         }
     }
+
     @GetMapping
     public String showDesignForm(Model model) {
         model.addAttribute("taco", new Taco());
         return "design";
     }
 
-    private Iterable<Ingredient> filterByType(
-            List<Ingredient> ingredients, Type type) {
+    @PostMapping
+    public String processTaco(Taco taco) {
+        // Save the taco
+        // We'll do this in chapter 3
+        log.info("Processing taco: " + taco);
+
+        return "redirect:/orders/current";
+    }
+
+    private Iterable<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
         return ingredients
                 .stream()
                 .filter(x -> x.getType().equals(type))
